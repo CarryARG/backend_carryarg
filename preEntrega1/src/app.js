f725083891b184561022c1dea9a1feda6fa1fdb7
CarryARG <lucaas_carranza@hotmail.com>
2023-05-27 18:15:05 -0300

    listo el 4to desafio

--- preEntrega1/src/app.js
+++ preEntrega1/src/app.js
@@ -1,23 +1,42 @@
 import express from "express";
+import handlebars from "express-handlebars";
+import __dirname from "./utils.js";
 import { ProductManager } from "./productManager.js";
 import { productManagerRouter } from "./routes/products.router.js";
 import { cartsRouter } from "./routes/carts.router.js";
+import { viewsRouter } from "./routes/views.router.js";
+import { Server } from "socket.io";
 const app = express();
 const port = 8080;
+
 app.use(express.urlencoded({ extended: true }));
 const productManager = new ProductManager();
 
+const httpServer = app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
+});
+
+const socketServer = new Server(httpServer);
+
+//configuracion del motor handlebars
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
+
+app.use("/", viewsRouter);
+
+socketServer.on("connection", async (socket) => {
+  console.log("Nuevo cliente conectado");
+  const products = await productManager.getProducts();
+  socket.emit("products", products);
+});
+
 //ingresar a http://localhost:8080/products :)
 // endpoints to routes
-
 app.use("/api/products", productManagerRouter);
-
-app.use("/api/carts/", cartsRouter);
+app.use("/api/carts", cartsRouter);
 
 app.get("*", (req, res) => {
   res.status(404).send({ status: "error", data: "Page not found" });
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port http://localhost:${port}`);
 });
